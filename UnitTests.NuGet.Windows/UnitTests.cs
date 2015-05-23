87075f168ef55093f8548c7347fb230276513156
mmaitre314 <mmaitre314@users.noreply.github.com>
2015-05-22 18:47:28 -0700

    Add NuGet package tests and fix missing package dependency

---
+++ UnitTests.NuGet.Windows/UnitTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using IpCamera;
+using MediaCaptureReader;
+using System.Threading.Tasks;
+
+namespace UnitTests.NuGet.Windows
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public async Task CS_W_N_CreateFromMediaCaptureAsync()
+        {
+            Logger.LogMessage("Creating NullMediaCapture");
+            var camera = NullMediaCapture.Create();
+            await camera.InitializeAsync();
+
+            Logger.LogMessage("Creating CameraServer");
+            using (var server = await CameraServer.CreateFromMediaCaptureAsync(camera))
+            {
+                Assert.IsTrue(server.Port > 0);
+                Assert.IsTrue(server.IPAddresses.Count >= 1);
+            }
+        }
+    }
+}
---
+++ UnitTests.NuGet.WindowsPhone/UnitTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Threading.Tasks;
+using MediaCaptureReader;
+using IpCamera;
+
+namespace UnitTests.NuGet.WindowsPhone
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public async Task CS_WP_N_CreateFromMediaCaptureAsync()
+        {
+            Logger.LogMessage("Creating NullMediaCapture");
+            var camera = NullMediaCapture.Create();
+            await camera.InitializeAsync();
+
+            Logger.LogMessage("Creating CameraServer");
+            using (var server = await CameraServer.CreateFromMediaCaptureAsync(camera))
+            {
+                Assert.IsTrue(server.Port > 0);
+                Assert.IsTrue(server.IPAddresses.Count >= 1);
+            }
+        }
+    }
+}
